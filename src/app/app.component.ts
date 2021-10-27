b2c8e6ab7e866b02ae5857600edd452b5f3aaa27
Geoff Fox <fox.geoffrey@gmail.com>
2021-10-27 12:56:08 -0700

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'ang-mat-list';
+  techList = [
+    {id: 101, lang: 'Java'},
+    {id: 102, lang: 'Angular'},
+    {id: 103, lang: 'Hibernate'}
+  ];
+  constructor(private formBuilder: FormBuilder) { }
+  techForm = this.formBuilder.group({
+    selectedTech: ''
+  });
+  onFormSubmit() {
+    console.log(this.techForm.get('selectedTech')?.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
